7077bdee3f15913215aa45e87e26b3c612b58c8c
dfgeoghagan <dfgeoghagan@valdosta.edu>
2017-09-07 17:08:17 -0400

    Dustin's Three Books

--- db/GenerateBooks.sql
+++ db/GenerateBooks.sql
@@ -1,2 +1,5 @@
 #ISBN Title Year Count
 INSERT INTO BOOKS VALUES (0192835084, 'The Adventures of Sherlock Holmes', 1998, 1);
+INSERT INTO BOOKS VALUES (3442474248,'World War Z: An Oral History of the Zombie War', 2006,2);
+INSERT INTO BOOKS VALUES (978-0062024039,'Divergent', 2012,3);
+INSERT INTO BOOKS VALUES (978-0006499589,'A Secret Affair', 2007,4);
