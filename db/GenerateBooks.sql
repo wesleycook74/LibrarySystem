d3277097af97396325e85c4aa0a0b82b7f384d06
Wesley I Cook <wicook@valdosta.edu>
2017-09-07 17:29:48 -0400

    Removed comment

--- db/GenerateBooks.sql
+++ db/GenerateBooks.sql
@@ -5,7 +5,7 @@ INSERT INTO keywords VALUES(0192835084, 'Short story');
 INSERT INTO keywords VALUES(0192835084, 'Crime Fiction');
 INSERT INTO keywords VALUES(0192835084, 'Detective fiction');
 
-INSERT INTO BOOKS VALUES (3442474248,'World War Z: An Oral History of the Zombie War', 2006,2); #Not Accepting ISBN Needs fix
+INSERT INTO BOOKS VALUES (3442474248,'World War Z: An Oral History of the Zombie War', 2006,2);
 INSERT INTO BOOKS VALUES (978-0062024039,'Divergent', 2012,3);
 INSERT INTO BOOKS VALUES (978-0006499589,'A Secret Affair', 2007,4);
 
