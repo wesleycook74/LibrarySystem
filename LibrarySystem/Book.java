d15930a300a845615dd091308530390acf634de9
Wesley Cook <wesleycook74@gmail.com>
2017-10-02 09:31:16 -0400

    Started database query for search

--- LibrarySystem/Book.java
+++ LibrarySystem/Book.java
@@ -6,9 +6,10 @@ public class Book {
 	private String year;
 	private String title;
 	private String[] authors;
+	private int count;
 	private String[] keywords;
 
-	public Book(String isbn, int id, String year, String title, String[] authors, String[] keywords) {
+	public Book(int id, String isbn, String year, String title, String[] authors, String[] keywords) {
 		this.isbn = isbn;
 		this.id = id;
 		this.year = year;
@@ -17,16 +18,34 @@ public class Book {
 		this.keywords = keywords;
 	}
 
-	public static Book[] searchBooksByTitle(String query) {
+	public static Book[] searchBooksByTitle(String title) {
+		Connection con = Database.getConnection();
+		String query = "";
+		try {
+			//create the prepared statement
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+			}
+			rs.close();
+			ps.close();
+		}
+		catch (SQLException se) {
+			se.printStackTrace();
+		}
 		return null;
 	}
 
-	public static Book[] searchBooksByAuthor(String query) {
+	public static Book[] searchBooksByYear(String year) {
+		return null;
+	}
+
+	public static Book[] searchBooksByAuthor(String author) {
 		return null;
 	}
 
 	public boolean isAvailable() {
-
 		return false;
 	}
 }
