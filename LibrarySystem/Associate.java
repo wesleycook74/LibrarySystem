d0052672860076be5540722e043ef05514cdc009
Chloe Kimble <chloeakimble@gmail.com>
2017-10-02 13:23:42 -0400

    worked on createMember

--- LibrarySystem/Associate.java
+++ LibrarySystem/Associate.java
@@ -1,5 +1,14 @@
 public class Associate extends Member {
-	public void createMember(){
+	public void createMember(String firstName, String lastName, String middleInitial, 
+                              String memberID, String phoneNumber, String userName, String password;){
+           this.firstName = firstName;
+           this.lastName = lastName;
+           this.middleInitial = middleInitial;
+           this.memberID = memberID;
+           this.phoneNumber = phoneNumber;
+           this.userName = userName;
+           this.password = password;
+	}
 
 	}
 }
