7a45aa34716f4f62e98c8b78efddd1c62c3c62eb
Wesley Cook <wesleycook74@gmail.com>
2017-10-02 16:58:32 -0400

    Removed unnecessary imports from tester

--- LibrarySystem/Tester.java
+++ LibrarySystem/Tester.java
@@ -1,12 +1,4 @@
 import java.util.ArrayList;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Tester {
 
