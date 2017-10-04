36dc2b197acfc67009bae6ba6e23e6f8612b6cb4
Dustin Geoghagan <dfgeoghagan@valdosta.edu>
2017-10-04 19:17:50 +0000

    Fixed the merge problem

--- LibrarySystem/Tester.java
+++ LibrarySystem/Tester.java
@@ -1,11 +1,22 @@
 import java.util.ArrayList;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Member mem1 = new Member("Chloe", "A", "Kimble", "1500 N Patterson St Valdosta GA", "123456789", "cakimble", "password");
+		Associate ass1 = new Associate ("Arron", "T", "Croft", "123 Croft Rd Valdosta GA", "9998885555", "atcroft" , "somepassword" );
+		
 		ArrayList<BookDetail> bookDetails = Search.searchBooksByTitle("The");
 		for(BookDetail bd : bookDetails) {
-			System.out.println(bd);
+			System.out.println(bd.getTitle());
 		}
 	}
 
