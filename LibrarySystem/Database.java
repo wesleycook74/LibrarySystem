bf46a6ab2978c0a71fbb3aaa3eadd55d0aee9e63
arroncroft <arroncroft@hotmail.com>
2017-10-04 19:25:00 -0400

    tested db connection on home pc

--- LibrarySystem/Database.java
+++ LibrarySystem/Database.java
@@ -1,10 +1,10 @@
 import java.sql.*;
 public class Database {
 
-	// For opening Conections to the database
+	// For opening Connections to the database
 	public static Connection getConnection() {
 		Connection con = null;
-		//	JDBC URL, username and password of MySQL server
+		//JDBC URL, username and password of MySQL server
 		String url = "jdbc:mysql://localhost:3306/library?useSSL=false";
 		String user = "root";
 		String password = "root";
