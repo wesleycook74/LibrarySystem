84aaac7dd6753646b1f8fadfedd9fee4ef34c2e8
Arron Croft <arroncroft@hotmail.com>
2017-11-15 15:49:24 -0500

    Improved aesthetics of Member class

--- LibrarySystem/Member.java
+++ LibrarySystem/Member.java
@@ -64,7 +64,6 @@ public class Member {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public Member(int memID) {
@@ -102,7 +101,6 @@ public class Member {
 		} catch (SQLException se) {
 			se.printStackTrace();
 		}
-
 	}
 
 	public String getFirstName() {
@@ -226,7 +224,7 @@ public class Member {
 
 
 	public void renewBook(Book book) {
-
+	//to do
 	}
 
 	public void placeHold(BookDetail bookDetail) {
@@ -253,13 +251,12 @@ public class Member {
 	}
 
 	public void reportLost(Book book) {
-
+	//to do
 	}
 
 	public void suspendAccount() {
 		String update = "UPDATE MEMBERS \n" + "SET Is_active=FALSE \n" + "WHERE MemberID=" + memberID + ";";
 		Database.runUpdate(update);
-
 	}
 
 	public void activateAccount() {
