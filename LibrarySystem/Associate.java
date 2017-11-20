55baa47c2ffcdc9e7b65c7d2cd784fe79bbcb229
Wesley Cook <wesleycook74@gmail.com>
2017-11-20 16:13:59 -0500

    Changed adding members/associates

--- LibrarySystem/Associate.java
+++ LibrarySystem/Associate.java
@@ -7,23 +7,22 @@ import java.sql.SQLException;
 
 public class Associate extends Member {
 
-	public Associate(String firstName, String middleInitial, String lastName, String address, String phoneNumber,
-			String userName, String password) {
-		super(firstName, middleInitial, lastName, address, phoneNumber, userName, password);
-		//sendassociatetoDB(firstName, middleInitial, lastName, address, phoneNumber, userName, password);
-
+	public Associate(int id)
+	{
+		super(id);
 	}
-	
-	
-	public void sendassociatetoDB(String firstName, String middleInitial, String lastName, String address, String phoneNumber,
-			String userName, String password){
-		
-		int memberID=0;
-		
+
+	public Associate(String username)
+	{
+		super(username);
+	}
+
+
+	public void createMember(String firstName, String middleInitial, String lastName, String address, String phoneNumber,
+							String userName, String password){
 		Connection con = Database.getConnection();
 
-		String query = "insert into MEMBERS (Fname, Minit, Lname, Address, PhoneNumber, Username, Password, Is_active)"
-				+ " values (?, ?, ?, ?, ?, ? ,?, ?)";
+		String query = "insert into MEMBERS values (?, ?, ?, ?, ?, ? ,?, ?)";
 
 		PreparedStatement ps2 = null;
 		try {
@@ -42,71 +41,7 @@ public class Associate extends Member {
 			e.printStackTrace();
 		}
 
-		String getmemid = "SELECT MemberID\n" + "FROM MEMBERS BD\n" + "WHERE Username = '" + userName + "'";
-
-		try {
-			PreparedStatement mid = con.prepareStatement(getmemid);
-			ResultSet rs = mid.executeQuery();
-
-			// int id = ((Integer) rs.getObject(1)).intValue();
-			// int id = Integer.parseInt(rs.getObject(1).toString());
-			while (rs.next()) {
-				memberID = rs.getInt("MemberID");
-			}
-			rs.close();
-			con.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		String query3 = "insert into ASSOCIATES (MemberID, Manager)" + " values (?, ?)";
-		try {
-
-			PreparedStatement ps3 = con.prepareStatement(query3);
-			ps2.setInt(1, memberID);
-			ps2.setInt(2, 0);
-
-			ps2.execute();
-			con.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
-	
-	
-	public Member createMember(String firstName, String middleInitial, String lastName, String address, String phoneNumber,
-			String userName, String password){
-		Member mem = new Member( firstName,  middleInitial, lastName,  address,  phoneNumber, userName, password);
-		return mem;
-	}
-	
-	public void sendmembertoDB(Member mem){
-		Connection con = Database.getConnection();
-
-		String query = "insert into MEMBERS (Fname, Minit, Lname, Address, PhoneNumber, Username, Password, Is_active)"
-				+ " values (?, ?, ?, ?, ?, ? ,?, ?)";
-
-		PreparedStatement ps2 = null;
-		try {
-			ps2 = con.prepareStatement(query);
-			ps2.setString(1, mem.getFirstName());
-			ps2.setString(2, mem.getMiddleInitial());
-			ps2.setString(3, mem.getLastName());
-			ps2.setString(4, mem.getAddress());
-			ps2.setString(5, mem.getPhoneNumber());
-			ps2.setString(6, mem.getUserName());
-			ps2.setString(7, mem.getPassword());
-			ps2.setBoolean(8, true);
-
-			ps2.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		String getmemid = "SELECT MemberID\n" + "FROM MEMBERS BD\n" + "WHERE Username = '" + mem.getUserName() + "'";
+		String getmemid = "SELECT MemberID\n" + "FROM MEMBERS\n" + "WHERE Username = '" + userName + "'";
 
 		try {
 			PreparedStatement mid = con.prepareStatement(getmemid);
