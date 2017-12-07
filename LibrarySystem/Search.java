22355e2bbb570591e6ef14c2de5f0d7ab5340d5f
arroncroft <arroncroft@hotmail.com>
2017-12-06 21:38:08 -0500

    small aesthetic changes and finished search by author

--- LibrarySystem/Search.java
+++ LibrarySystem/Search.java
@@ -67,6 +67,22 @@ public class Search {
 	}
 
 	public static ArrayList<Book> searchBooksByAuthor(String author) {
-		return null;
+		ArrayList<Book> books = new ArrayList<>();
+		Connection con = Database.getConnection();
+		String query = "SELECT B.ISBN\n" + "FROM BOOKS B, AUTHORS A\n" + "WHERE B.ISBN = A.ISBN AND A.AName LIKE '%" + author + "%'";
+		try {
+			// create the prepared statement
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				books.add(new Book(rs.getString("ISBN")));
+			}
+			rs.close();
+			ps.close();
+			con.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		return books;
 	}
 }
